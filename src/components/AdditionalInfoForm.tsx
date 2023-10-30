8c724f2c84b316890e1dfa9f248b70b751936285
amitsatpute-pyjs <amitkumar.satpute@gslab.com>
2023-10-30 19:23:37 +0530

    cashinflow and outflow added

--- src/components/AdditionalInfoForm.tsx
+++ src/components/AdditionalInfoForm.tsx
@@ -18,7 +18,9 @@ const AdditionalInfoForm = (props: any) => {
         education: "",
         jobType: "",
         dependents: "",
-        loanAmount: ""
+        loanAmount: "",
+        cashinflow:"",
+        cashoutflow:""
     })
 
     // useEffect(() => {
@@ -112,6 +114,17 @@ const AdditionalInfoForm = (props: any) => {
                                 <option value={1}>self-employed</option>
                             </select>
                         </div>
+                    </div>
+                    <div className="grid md:grid-cols-2 md:gap-6">
+                        <div className="relative z-0 w-full mb-6 group">
+                            <input value={formValues.cashinflow} type="text" name="floating_cashinflow" id="floating_cashinflow" onChange={(e) => setFormValues({ ...formValues, cashinflow: e.target.value })} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Cash inflow</label>
+                        </div>
+                        <div className="relative z-0 w-full mb-6 group">
+                            <input value={formValues.cashoutflow} type="text" name="floating_cashoutflow" id="floating_cashoutflow" onChange={(e) => setFormValues({ ...formValues, cashoutflow: e.target.value })} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Cash outflow</label>
+                        </div>
+                       
                     </div>
                     <div className="grid md:grid-cols-3 md:gap-6">
                         <div className="relative z-0 w-full mb-6 group">
